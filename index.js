69bfb8f662cbfcf497bfcd980ff88ea3ea3a5ccb
Jakob Salomonsson <jakob.salomonsson@gmail.com>
2020-10-10 18:06:13 +0200

    updating list items

--- index.js
+++ index.js
@@ -12,12 +12,22 @@ function shoppingListFunctionality() {
         const item = $(this).find('#shopping-list-entry');
 
         // Add the entered item to the list
-        $('.shopping-list').append(`<li><span class="shopping-item">${item.val()}</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button> <button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>`);
-
+        $('.shopping-list').append(`
+                                    <li>
+                                        <span class="shopping-item">${item.val()}</span>
+                                        <div class="shopping-item-controls">
+                                            <button class="shopping-item-toggle">
+                                                <span class="button-label">check</span>
+                                            </button>
+                                            <button class="shopping-item-delete">
+                                                <span class="button-label">delete</span>
+                                            </button>
+                                        </div>
+                                    </li>`);
+        
         // Reset the entered item
         item.val("");
     });
-
 
     // Functionality for removing an item.
     $('ul').on('click', '.shopping-item-delete', function(event) {
@@ -28,6 +38,7 @@ function shoppingListFunctionality() {
     // Functionality for checking and unchecking items in the list
     $('ul').on('click', '.shopping-item-toggle', function(event) {
         $(this).closest('li').find('span.shopping-item').toggleClass('shopping-item__checked');
+        debugger;
     });
 };
 
